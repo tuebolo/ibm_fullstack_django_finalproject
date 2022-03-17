700837e2a3e96ed7b2fbd2903312b9264da9c3ac
Isabelle Russell <isabelle.russell@outlook.com>
2022-03-17 16:19:29 +0100

    First question submission form

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -1,18 +1,26 @@
 from django.contrib import admin
 # <HINT> Import any new Models here
-from .models import Course, Lesson, Instructor, Learner
+from .models import Course, Lesson, Instructor, Learner, Question, Choice
 
 # <HINT> Register QuestionInline and ChoiceInline classes here
 
 
 class LessonInline(admin.StackedInline):
     model = Lesson
-    extra = 5
+    extra = 1
+
+class QuestionInline(admin.StackedInline):
+    model = Question
+    extra = 1
+
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 1
 
 
 # Register your models here.
 class CourseAdmin(admin.ModelAdmin):
-    inlines = [LessonInline]
+    inlines = [LessonInline, QuestionInline]
     list_display = ('name', 'pub_date')
     list_filter = ['pub_date']
     search_fields = ['name', 'description']
@@ -22,9 +30,16 @@ class LessonAdmin(admin.ModelAdmin):
     list_display = ['title']
 
 
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInline]
+
 # <HINT> Register Question and Choice models here
 
 admin.site.register(Course, CourseAdmin)
 admin.site.register(Lesson, LessonAdmin)
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
+
 admin.site.register(Instructor)
 admin.site.register(Learner)
